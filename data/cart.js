fc410677d44e30feab97da7796fb834f44ceadb9
PinkPanther21 <moyeon2109@gmail.com>
2024-07-24 18:21:28 +0500

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = [
     {
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         Quantity : 2
@@ -9,6 +11,11 @@ export let cart = [
 
     }
 ];
+} 
+
+function saveToStorage() {
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -27,9 +34,12 @@ export function addToCart(productId){
         Quantity: 1
       });
     }
+    saveToStorage();
 }
 export function removeFromCart(productId){
    const newCart= [];
+
+   
 
    cart.forEach((cartItem)=>{
     if(cartItem.productId !== productId){
@@ -37,4 +47,5 @@ export function removeFromCart(productId){
     }
    });
    cart = newCart;
+   saveToStorage();
 }
