8bccbd54d26bd05a232c4fdbe9634d2412b4da66
PinkPanther21 <moyeon2109@gmail.com>
2024-07-24 15:14:33 +0500

    delete product by clicking it

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utilities/money.js";
 
@@ -38,7 +38,7 @@ if(product.id === productId){
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -94,3 +94,11 @@ if(product.id === productId){
 });
 document.querySelector('.js-order-summary')
 .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link)=> {
+link.addEventListener('click', ()=>{
+  const productId = link.dataset.productId;
+  removeFromCart(productId);
+});
+});
